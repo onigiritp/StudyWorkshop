873ddcdd847640a700a5d172465d224fe1cee9e9
onigiritp <46705025+onigiritp@users.noreply.github.com>
2020-09-02 20:20:38 +0900

    素数計算

---
+++ sosu.py
@@ -0,0 +1,21 @@
+
+
+def main():
+    num = int(input("いくつまでの素数を足しますか・・・"))
+    sosu(num)
+
+def sosu(num):
+
+    sum_data = 0
+    for i in range(2,num):
+        if i == 2:
+            sum_data += i
+        else:
+            for j in range(2,i):
+                if i % j == 0:
+                    break
+            else:
+                sum_data += i
+    print(sum_data)
+
+main()
